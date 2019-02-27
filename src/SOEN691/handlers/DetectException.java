603108e932b5ae687ad3a5c26c1e2585ebc6fba4
lqw1111 <369487836luo@gmail.com>
2019-02-26 23:31:50 -0500

    lqw

--- src/SOEN691/handlers/DetectException.java
+++ src/SOEN691/handlers/DetectException.java
@@ -17,29 +17,29 @@ public class DetectException extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IWorkspaceRoot root = workspace.getRoot();
-		
+
 		IProject[] projects = root.getProjects();
-		
+
 		detectInProjects(projects);
-		
+
 		SampleHandler.printMessage("DONE DETECTING");
-		
+
 		return null;
 	}
-	
+
 	private void detectInProjects(IProject[] projects) {
 		for(IProject project : projects) {
 			SampleHandler.printMessage("DETECTING IN: " + project.getName());
 			ExceptionFinder exceptionFinder = new ExceptionFinder();
-			
+
 			try {
 				// find the exceptions and their methods
 				exceptionFinder.findExceptions(project);
-				
-				
+
+
 			} catch (JavaModelException e) {
 				e.printStackTrace();
-			}	
+			}
 	}
 	}
 }
--- src/SOEN691/visitors/CatchClauseVisitor.java
+++ src/SOEN691/visitors/CatchClauseVisitor.java
@@ -23,44 +23,113 @@ public class CatchClauseVisitor extends ASTVisitor{
 	HashSet<CatchClause> multipleLineCatches = new HashSet<CatchClause>();
 	HashSet<CatchClause> destructiveWrappingCatches = new HashSet<CatchClause>();
 	HashSet<CatchClause> overCatches = new HashSet<CatchClause>();
-	
+
 	public CatchClauseVisitor() {}
-	
+
 	public CatchClauseVisitor(HashSet<CatchClause> previouslyFoundCatches) {
 		indebtCatches.addAll(previouslyFoundCatches);
 	}
 
-	
+
+	public List<String> FindRuntimeExceptions(MethodInvocation mInvocation) throws JavaModelException {
+		IMethodBinding imb = mInvocation.resolveMethodBinding().getMethodDeclaration();
+		List<String> exceptionList = new ArrayList<>();
+		IMethod imethod = (IMethod) imb.getJavaElement();
+		ISourceRange javadocRange;
+		javadocRange = imethod.getJavadocRange();
+		if (javadocRange != null) {
+			String javadocString = getJavadocFast(imethod);
+			exceptionList.addAll(FindExceptionsInJavadoc(javadocString));
+		}
+		return exceptionList;
+
+	}
+	public List<String> FindRuntimeExceptions(IMethodBinding imb ) throws JavaModelException {
+		List<String> exceptionList = new ArrayList<>();
+		IMethod imethod = (IMethod) imb.getJavaElement();
+		if(imethod == null) {
+			return exceptionList;
+		}
+		ISourceRange javadocRange;
+		javadocRange = imethod.getJavadocRange();
+		if (javadocRange != null) {
+			String javadocString = getJavadocFast(imethod);
+			exceptionList.addAll(FindExceptionsInJavadoc(javadocString));
+		}
+		return exceptionList;
+
+	}
+
+	public List<String> FindNonRuntimeExceptions(MethodInvocation mInvocation) throws JavaModelException {
+		IMethodBinding imb = mInvocation.resolveMethodBinding().getMethodDeclaration();
+		List<String> exceptionList = new ArrayList<>();
+		//Add all Non-Runtime exception in try block
+		//Example:  void print() throw XXXException
+		for(ITypeBinding b:imb.getExceptionTypes()) {
+			String str = b.getQualifiedName();
+			if(str.contains(".")) {
+				String ss ;
+				int flag = str.lastIndexOf(".")+1;
+				ss = str.substring(flag, str.length());
+				exceptionList.add(ss);
+
+			}
+			else {
+				exceptionList.add(str);
+			}
+
+		}
+
+		return exceptionList;
+	}
+	public List<String> FindNonRuntimeExceptions(IMethodBinding imb) throws JavaModelException {
+		List<String> exceptionList = new ArrayList<>();
+		//Add all Non-Runtime exception in try block
+		//Example:  void print() throw XXXException
+		for(ITypeBinding b:imb.getExceptionTypes()) {
+			String str = b.getQualifiedName();
+			if(str.contains(".")) {
+				String ss ;
+				int flag = str.lastIndexOf(".")+1;
+				ss = str.substring(flag, str.length());
+				exceptionList.add(ss);
+
+			}
+			else {
+				exceptionList.add(str);
+			}
+
+		}
+
+		return exceptionList;
+	}
+
+
 	@Override
 	public boolean visit(CatchClause node) {
-		
+
 		//catch exceptions set in try block (init)
-		HashSet<ITypeBinding> tryExceptionSet = new HashSet<ITypeBinding>();
+//		HashSet<String> tryExceptionStringSet = new HashSet<String>();
+
 		// catch exception type
-		ITypeBinding exceptionTypeInCatch = 
+		ITypeBinding exceptionTypeInCatch =
 				node.getException().getType().resolveBinding();
-		
+		String wholeException = exceptionTypeInCatch.getQualifiedName();
+		String exceptionNameInCatch = wholeException.substring(wholeException.lastIndexOf(".")+1,wholeException.length());
+
 		// get try block
 		TryStatement tryStatement = (TryStatement)node.getParent();
 		Block tryBlock = tryStatement.getBody();
-		List<ASTNode> tryBodies = tryBlock.statements();
-		
-		for(ASTNode nn: tryBodies) {
-			// each statement in try
-			if( nn instanceof VariableDeclarationStatement) {
-				//TODO 
-			}
-			
-			if (nn instanceof ExpressionStatement) {
-				ExpressionStatement ex = (ExpressionStatement) nn;
-				MethodInvocation mInvocation  = (MethodInvocation)ex.getExpression();
-				IMethodBinding mDeclaration = mInvocation.resolveMethodBinding().getMethodDeclaration();
-				for(ITypeBinding b:mDeclaration.getExceptionTypes()) {
-					tryExceptionSet.add(b);
-				}
+		MethodInvocationInTryVisitor mVisitor = new MethodInvocationInTryVisitor();
+
+		tryBlock.accept(mVisitor);
+		//result of exceptions in tryBlock is in mVisitor.ResultExceptionSet
+		//All exception
+
+
 
 			}
-			
+
 		}
 		//to compare exceptions between try and block
 		boolean overcatch = true;
@@ -68,45 +137,181 @@ public class CatchClauseVisitor extends ASTVisitor{
 			if(itype.equals(exceptionTypeInCatch)) {
 				overcatch = false;
 			}
-			
+
 		}
+
+
 		if(overcatch) {
 			overCatches.add(node);
 		}
-		
+
+		//MuitipleLine logs and destructive wrapping catches
 		int countOfLog = 0;
-		
-		MethodInvocationVisitor visitMethodInvocation = new MethodInvocationVisitor();
+
 		Block block = node.getBody();
 
 		List<ASTNode> bodies = block.statements();
 		for(ASTNode nn: bodies) {
 			if (nn instanceof ExpressionStatement) {
 				ExpressionStatement ex = (ExpressionStatement) nn;
-				MethodInvocation mInvocation  = (MethodInvocation)ex.getExpression();
-				ITypeBinding type = mInvocation.resolveMethodBinding().getDeclaringClass();
-				if (type.getQualifiedName().contentEquals("java.util.logging.Logger")) {
-					countOfLog++;
+				if(ex.getExpression() instanceof MethodInvocation) {
+					MethodInvocation mInvocation  = (MethodInvocation)ex.getExpression();
+
+					ITypeBinding type = mInvocation.resolveMethodBinding().getDeclaringClass();
+
+
+
+					if (type.getQualifiedName().contentEquals("java.util.logging.Logger")) {
+						String name = mInvocation.getName().toString();
+						if(CheckLogLevel(name))
+							countOfLog++;
+					}
+
 				}
-				
+
 			}
 			else if (nn instanceof ThrowStatement) {
 				destructiveWrappingCatches.add(node);
-				
 			}
 		}
-		
+
 		if (countOfLog >1) {
 			multipleLineCatches.add(node);
 		}
 		return super.visit(node);
 	}
-	
-	
+
+
 	public HashSet<CatchClause> getMultipleLineLogCatches() {
 		return multipleLineCatches;
 	}
-	
+
+	public  boolean CheckLogLevel(String name) {
+		if(name.equals("info")) {
+			return true;
+		}
+		if(name.equals("warning")) {
+			return true;
+		}
+		if(name.equals("severe")) {
+			return true;
+		}
+		if(name.equals("log")) {
+			return true;
+		}
+		if(name.equals("finest")) {
+			return true;
+		}
+		if(name.equals("finer")) {
+			return true;
+		}
+		if(name.equals("fine")) {
+			return true;
+		}
+		if(name.equals("exiting")) {
+			return true;
+		}
+		if(name.equals("entering")) {
+			return true;
+		}
+		if(name.equals("config")) {
+			return true;
+		}
+		if(name.equals("debug")) {
+			return true;
+		}
+		return false;
+
+	}
+
+
+	private static String getJavadocFast(IMember member) throws JavaModelException {
+		IBuffer buffer = member.getOpenable().getBuffer();
+
+		ISourceRange javadocRange = member.getJavadocRange();
+		String javadocText = buffer.getText(javadocRange.getOffset(), javadocRange.getLength());
+
+			javadocText = javadocText.replaceAll("^/[*][*][ \t]*\n?", "");  // Filter starting /**
+			javadocText = javadocText.replaceAll("\n?[ \t]*[*]/$", "");  // Filter ending */
+			javadocText = javadocText.replaceAll("^\\s*[*]", "\n");  // Trim leading whitespace.
+			javadocText = javadocText.replaceAll("\n\\s*[*]", "\n");  // Trim whitespace at beginning of line.
+			javadocText = javadocText.replaceAll("<[^>]*>", "");  // Remove html tags.
+			javadocText = javadocText.replaceAll("[{]@code([^}]*)[}]", "$1");  // Replace {@code foo} blocks with foo.
+			javadocText = javadocText.replaceAll("&nbsp;", " ").replaceAll("&lt;", "<").replaceAll("&gt;", ">").replaceAll("&quot;", "\"");  // Replace html formatting.
+
+
+//		javadocText = Flags.toString(member.getFlags()) + " " + JavaElement.getElementLabel(member, JavaElementLabels.M_PRE_RETURNTYPE | JavaElementLabels.M_PARAMETER_NAMES | JavaElementLabels.M_PARAMETER_TYPES | JavaElementLabels.F_PRE_TYPE_SIGNATURE) + "\n" + javadocText;
+		return javadocText;
+	}
+
+	public List<String> FindExceptionsInJavadoc(String javadocText){
+		List<String> exceptionList = new ArrayList<>();
+		String[] array = javadocText.split("\n");
+		for(int i =0;i<array.length;i++) {
+			if(array[i].contains("@throws")) {
+				String[] temp = array[i].split(" ");
+				for(String ss: temp) {
+					if(ss.contains("Exception")) {
+						exceptionList.add(ss);
+						break;
+					}
+				}
+
+
+			}
+		}
+		return exceptionList;
+	}
+	public MethodDeclaration FindMethodDeclaration(MethodInvocation node) {
+
+		IMethodBinding binding = (IMethodBinding) node.getName().resolveBinding();
+		ICompilationUnit unit = (ICompilationUnit) binding.getJavaElement().getAncestor( IJavaElement.COMPILATION_UNIT );
+		if ( unit == null ) {
+			return null;
+		   // not available, external declaration
+		}
+		ASTParser parser = ASTParser.newParser( AST.JLS8 );
+		parser.setKind( ASTParser.K_COMPILATION_UNIT );
+		parser.setSource( unit );
+		parser.setResolveBindings( true );
+		CompilationUnit cu = (CompilationUnit) parser.createAST( null );
+		MethodDeclaration decl = (MethodDeclaration)cu.findDeclaringNode( binding.getKey() );
+
+		return decl;
+
+
+	}
+
+	public MethodDeclaration FindMethodDeclaration(IMethodBinding binding ) {
+		if(binding == null) {
+			return null;
+		}
+		IJavaElement ije = binding.getJavaElement();
+		if(ije == null) {
+			return null;
+		}
+		Object obj = ije.getAncestor( IJavaElement.COMPILATION_UNIT );
+
+		ICompilationUnit unit;
+		if(obj != null) {
+			unit = (ICompilationUnit)obj;
+		}
+		else {
+			return null;
+		}
+
+		ASTParser parser = ASTParser.newParser( AST.JLS8 );
+		parser.setKind( ASTParser.K_COMPILATION_UNIT );
+		parser.setSource( unit );
+		parser.setResolveBindings( true );
+		CompilationUnit cu = (CompilationUnit) parser.createAST( null );
+		MethodDeclaration decl = (MethodDeclaration)cu.findDeclaringNode( binding.getKey() );
+
+		return decl;
+
+
+	}
+
 	public HashSet<CatchClause> getDestructiveWrappingCatches() {
 		return destructiveWrappingCatches;
 	}
